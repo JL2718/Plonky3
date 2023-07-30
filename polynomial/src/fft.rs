0ccceebece3d86f7e37cf1c9f2553e5d92440339
JL2718 <JL2718@github>
2023-07-30 01:15:38 -0400

    fixed types

--- polynomial/src/fft.rs
+++ polynomial/src/fft.rs
@@ -1,28 +1,27 @@
-use p3_field::{TwoAdicField};
+use p3_field::TwoAdicField;
 
 pub fn fft
-<F:TwoAdicField,const N:usize>
-(vals:&mut [F;N],root:Option<F>,inverse:Option<bool>)
+<F:TwoAdicField,const N:usize,const INV:bool>
+(vals:&mut [F;N])
 {
     /*
     In-place FFT
     Cooley-Tukey FFT Algorithm on the input 'vals' using the root of unity 'root'
     TODO: look into eliminating the bit-reversal requirement
     */
-    const _: () = assert!(N.is_power_of_two(), "array size must be a power of two");
-    const LN:usize=N.trailing_zeros() as usize;
+    static_assert_power_of_two::<N>();
     // root: 2^Nth root of unity or inverse root if inverse
-    let root:F = (|r:F|if inverse {r.inverse()} else {r})(
-        root or F::power_of_two_generator().exp_power_of_2((F::TWO_ADICITY as isize  - LN as isize) as usize)
+    let root:F = (|r:F|if INV {r.inverse()} else {r})(
+        F::power_of_two_generator().exp_power_of_2((F::TWO_ADICITY as isize  - N.trailing_zeros() as isize) as usize)
     );
-    debug_assert!(root.exp_power_of_2(LN) == F::ONE);
+    debug_assert!(root.exp_power_of_2(N.trailing_zeros() as usize) == F::ONE);
     // rr: sequence of root squares from {2^-1, 2^-2, 2^-4, ..., root=2^-N}
-    let rr:[F;LN]=(0..LN).scan(root,|ri,_|{ri*=ri;Some(*ri)}).collect::<Vec<F>>().try_into().unwrap();
+    let rr:Vec<F>=(0..N.trailing_zeros()).scan(root,|ri,_|{*ri*=*ri;Some(*ri)}).collect();
     // bit-reversal permutation
-    bit_reverse_permutation(&vals);
+    bit_reverse_permutation(vals);
     // Cooley-Tukey FFT Algorithm (in-place)
     for (i,r) in rr.iter().rev().enumerate(){
-        for j in (0..(1<<LN)).step_by(1<<(i+1)) {
+        for j in (0..N).step_by(1<<(i+1)) {
             let mut s = F::ONE;
             for k in j..j + (1<<i) {
                 let u = vals[k];
@@ -34,21 +33,25 @@ pub fn fft
         }
     }
     // divide by N if inverse
-    if inverse {
-        let inv = F::TWO.exp_u64(LN as u64).inverse();
-        for i in 0..(1<<LN) {
+    if INV {
+        let inv = F::TWO.exp_u64(N.trailing_zeros() as u64).inverse();
+        for i in 0..(1<<N.trailing_zeros()) {
             vals[i] *= inv;
         }
     }
 }
 
-pub const fn root_of_unity<F:TwoAdicField,N>()->F{
-    F::power_of_two_generator()
-    .exp_power_of_2((F::TWO_ADICITY as isize  - n.trailing_zeros() as isize) as usize)
+pub fn root_of_unity<F:TwoAdicField,const N:usize>()->F{
+    static_assert_power_of_two::<N>();
+    F::power_of_two_generator().exp_power_of_2((F::TWO_ADICITY as isize  - N.trailing_zeros() as isize) as usize)
 }
 
- pub core::num::reverse_bits<N:usize>(v:usize){
+fn reverse_bits<const N:usize>(v:usize)->usize{
     return v.reverse_bits() >> (usize::BITS - N.trailing_zeros());
+}
+
+const fn static_assert_power_of_two<const N:usize>(){
+    assert!(N.is_power_of_two(), "array size must be a power of two");
 }
 
 pub fn bit_reverse_permutation
@@ -58,7 +61,7 @@ pub fn bit_reverse_permutation
         Bit-reversal permutation
         Rearrange the input 'vals' in bit-reversal order
      */
-    const _: () = assert!(N.is_power_of_two(), "array size must be a power of two");
+    static_assert_power_of_two::<N>();
     for i in 0..N {
         let j = i.reverse_bits() >> (usize::BITS - N.trailing_zeros());
         if i < j {
@@ -93,9 +96,7 @@ mod tests_mersenne {
         let mut aa = [F::default();N];
         aa.iter_mut().for_each(|a| *a=F::new_real(rng.gen::<B>())); 
         let aa_0 = aa.clone();
-        bit_reverse(& mut aa);
         fft::<F,8,false>(& mut aa);
-        bit_reverse(& mut aa);
         fft::<F,8,true>(& mut aa);
         assert_eq!(aa,aa_0);
     }
@@ -105,7 +106,6 @@ mod tests_mersenne {
         let mut aa = [F::ZERO;N];
         aa[0] = F::ONE;
         let aa_0 = aa.clone();
-        bit_reverse(& mut aa);
         fft::<F,8,false>(& mut aa);
         let aa_1 = aa.clone();
         let aa_2 = dft::<F,8,false>(aa_0);
@@ -118,7 +118,6 @@ mod tests_mersenne {
         let mut aa = [F::default();N];
         aa.iter_mut().for_each(|a| *a=F::new_real(rng.gen::<B>())); 
         let aa_0 = aa.clone();
-        bit_reverse(& mut aa);
         fft::<F,8,false>(& mut aa);
         let aa_1 = aa.clone();
         let aa_2 = dft::<F,8,false>(aa_0);
@@ -131,7 +130,6 @@ mod tests_mersenne {
         let mut aa = [F::default();N];
         aa.iter_mut().for_each(|a| *a=F::new_real(rng.gen::<B>())); 
         let aa_0 = aa.clone();
-        bit_reverse(& mut aa);
         fft::<F,8,true>(& mut aa);
         let aa_1 = aa.clone();
         let aa_2 = dft::<F,8,true>(aa_0);
