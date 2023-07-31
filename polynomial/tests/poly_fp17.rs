0e09d344b0594fad1ad6df1d565314aa15a52948
JL2718 <JL2718@github>
2023-07-31 07:55:41 -0400

    polynomials arithmetic tests with fft and dft

---
+++ polynomial/tests/poly_fp17.rs
@@ -0,0 +1,53 @@
+#[cfg(test)]
+mod tests_poly_fp17 {
+    use rand::Rng;
+    use p3_field::AbstractField;
+    use polynomial::{AbstractPolynomialCoefficients,AbstractPolynomialEvaluations,AbstractCyclicPolynomial,AbstractPolynomial};
+    type F = polynomial::fp17::Fp17;
+    type PC = polynomial::coeffs::CyclicPolynomialCoefficients<F,8>;
+    type PE = polynomial::evals::CyclicPolynomialEvaluations<F,8>;
+    #[test]
+    fn test_coeff_add_sub(){
+        let mut rng = rand::thread_rng();
+        let mut aa:PC = rng.gen();
+        let mut bb:PC = rng.gen();
+        let cc = aa+bb;
+        let dd = cc-bb;
+        assert_eq!(aa,dd);
+    }
+    #[test]
+    fn test_fft_mul_random_half(){
+        let mut rng = rand::thread_rng();
+        let mut aa:PC = rng.gen();
+        let mut bb:PC = rng.gen();
+        for i in 4..8 {
+            aa.vals[i] = F::ZERO;
+            bb.vals[i] = F::ZERO;
+        }
+        let cc = aa*bb;
+        let dd = (aa.fft()*bb.fft()).ifft();
+        assert_eq!(cc,dd);
+    }
+    #[test]
+    fn test_fft_mul_random_full(){
+        let mut rng = rand::thread_rng();
+        let mut aa:PC = rng.gen();
+        let mut bb:PC = rng.gen();
+        let cc = aa*bb;
+        let dd = (aa.fft()*bb.fft()).ifft();
+        assert_eq!(cc,dd);
+    }
+    #[test]
+    fn test_dft_mul_random_half(){
+        let mut rng = rand::thread_rng();
+        let mut aa:PC = rng.gen();
+        let mut bb:PC = rng.gen();
+        for i in 4..8 {
+            aa.vals[i] = F::ZERO;
+            bb.vals[i] = F::ZERO;
+        }
+        let cc = aa*bb;
+        let dd = (aa.dft()*bb.dft()).idft();
+        assert_eq!(cc,dd);
+    }
+}
